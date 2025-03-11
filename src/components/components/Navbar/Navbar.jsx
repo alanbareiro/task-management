307378021a08bbfa68a08485af2cf35a2d30315d
wifrons <wifron@gmail.com>
2025-03-11 09:59:44 -0300

    Update navbar
    Update Logo

--- src/components/components/Navbar/Navbar.jsx
+++ src/components/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     return (
         <>
             <div className={styles[`navbar-${classname}`]}> {/* Usa styles para acceder a las clases */}
-                <span className={styles[`logo-${classname}`]}>Trend</span> {/* Usa styles para acceder a las clases */}
+                <span className={styles[`logo-${classname}`]}>Pyme Calendar</span> {/* Usa styles para acceder a las clases */}
                 <label className={styles.switch} htmlFor="chekswitchid"> {/* Usa htmlFor para asociar el label con el input */}
                     <input
                         type="checkbox"
